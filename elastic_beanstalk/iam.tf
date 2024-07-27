30bf5ea8eca235f0d0a888060244a588f22598d1
vikash <jhonslax.bs@gmail.com>
2024-07-27 17:54:18 +0530

    added github ci/cd, lock state

--- elastic_beanstalk/iam.tf
+++ elastic_beanstalk/iam.tf
@@ -1,41 +1,23 @@
+# Load the policies from the JSON file
+locals {
+  policies = jsondecode(file("iam_policies/policies.json"))
+}
+
+# IAM Role
 resource "aws_iam_role" "eb_instance_role" {
   name = "eb_instance_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+  assume_role_policy = jsonencode(local.policies.eb_instance_role_assume_policy)
 }
 
+# IAM Policy
 resource "aws_iam_policy" "eb_s3_access" {
   name        = "eb_s3_access"
   description = "Allows Elastic Beanstalk instances to access S3 bucket"
-  policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::my-django-app-bucket-unique-12345",
-          "arn:aws:s3:::my-django-app-bucket-unique-12345/deployment/*"
-        ]
-      }
-    ]
-  })
+  policy      = jsonencode(local.policies.eb_s3_access_policy)
 }
 
+# Attach AWS Managed Policies
 resource "aws_iam_role_policy_attachment" "eb_instance_policy_web_tier" {
   role       = aws_iam_role.eb_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
@@ -46,11 +28,13 @@ resource "aws_iam_role_policy_attachment" "eb_instance_policy_multicontainer_doc
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
 }
 
+# Attach Custom IAM Policy
 resource "aws_iam_role_policy_attachment" "eb_instance_policy_s3_access" {
   role       = aws_iam_role.eb_instance_role.name
   policy_arn = aws_iam_policy.eb_s3_access.arn
 }
 
+# IAM Instance Profile
 resource "aws_iam_instance_profile" "eb_instance_profile" {
   name = "eb_instance_profile"
   role = aws_iam_role.eb_instance_role.name
